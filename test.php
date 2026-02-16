9cd32883158e5237f26ab517cfb73d3e45a74201
Jerome-vThink <jerome@vthink.co.in>
2026-02-16 19:36:56 +0530

    Another test

--- test.php
+++ test.php
@@ -6,7 +6,6 @@ class UserController {
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        // ❌ SQL Injection vulnerability
         $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($this->db, $query);
 
@@ -18,12 +17,14 @@ class UserController {
     }
 
     public function calculateDiscount($amount) {
-        // ❌ Assignment instead of comparison
+
         if ($amount = 100) {
             return $amount * 0.10;
         }
 
         return 0;
     }
+
+    echo "Hello World";
 
     public function
